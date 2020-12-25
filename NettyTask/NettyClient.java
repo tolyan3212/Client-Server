c72c002d695efae88c1c1a74230d2cdc6ef7dcfe
tolyan3212 <62468484+tolyan3212@users.noreply.github.com>
2020-12-25 21:54:36 +0300

    Add files via upload

--- NettyTask/NettyClient.java
+++ NettyTask/NettyClient.java
@@ -1,19 +1,11 @@
 import java.io.*;
-
-import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.*;
 import io.netty.channel.*;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-  
+import io.netty.channel.nio.*;
+import io.netty.channel.socket.*;
+import io.netty.channel.socket.nio.*;
+import io.netty.handler.ssl.*;
+import io.netty.handler.ssl.util.*;
 import io.netty.handler.codec.*;
 import io.netty.handler.codec.string.*;
 
@@ -28,15 +20,19 @@ public final class NettyClient {
             Bootstrap b = new Bootstrap();
             b.group(group)
                 .channel(NioSocketChannel.class)
-                .option(ChannelOption.TCP_NODELAY, true)
                 .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
                             ChannelPipeline pipeline = ch.pipeline();
-                            pipeline.addLast("framer", new DelimiterBasedFrameDecoder(1234, Delimiters.lineDelimiter()));
-                            pipeline.addLast("decoder", new StringDecoder());
-                            pipeline.addLast("encoder", new StringEncoder());
-                            pipeline.addLast("handler", new NettyClientHandler());
+                            pipeline.addLast("framer",
+                                             new DelimiterBasedFrameDecoder
+                                             (1234, Delimiters.lineDelimiter()));
+                            pipeline.addLast("decoder",
+                                             new StringDecoder());
+                            pipeline.addLast("encoder",
+                                             new StringEncoder());
+                            pipeline.addLast("handler",
+                                             new NettyClientHandler());
 
                         }                
                     });
